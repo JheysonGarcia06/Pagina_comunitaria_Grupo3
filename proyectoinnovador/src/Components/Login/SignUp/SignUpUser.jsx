29c7d8fcab6780360e1783af0056b0231b08855f
hover10tsuna@outlook.com <hover10tsuna@outlook.com>
2024-12-03 14:06:40 -0500

    no message

--- proyectoinnovador/src/Components/Login/SignUp/SignUpUser.jsx
+++ proyectoinnovador/src/Components/Login/SignUp/SignUpUser.jsx
@@ -96,11 +96,7 @@ const SignUpUser = () => {
                 <input className="inputGroupInput" type="text" id="fullName" name="fullName" required />
                 <label className="inputGroupLabel" htmlFor="fullName">Nombre completo</label>
             </div>
-            <div>
-              <div>
-                <label htmlFor="city">Ciudad:</label>
-              </div>
-
+            <div className="inputGroup">
               <select
                 id="city"
                 value={selectedCity}
@@ -113,6 +109,7 @@ const SignUpUser = () => {
                   </option>
                 ))}
               </select>
+              <label className="inputGroupLabel" htmlFor="city">Ciudad</label>
             </div>
             <button type="submit">Registrarme</button>
           </section>
@@ -122,41 +119,27 @@ const SignUpUser = () => {
         <form >
           {entrepeneurRole && (
           <section>
-          <div>
-             <div>
-               <label htmlFor="userName">Nombre de Usuario:</label>
-             </div>
-             <input type="text" id="userName" name="userName" required />
+          <div className="inputGroup">
+             <input className="inputGroupInput" type="text" id="userName" name="userName" required />
+             <label className="inputGroupLabel" htmlFor="userName">Nombre de Usuario:</label>
            </div>
-            <div>
-              <div>
-                <label htmlFor="password">Contraseña:</label>
-              </div>
-              <input type="password" id="password" name="password" required />
+            <div className="inputGroup">
+              <input className="inputGroupInput" type="password" id="password" name="password" required />
+              <label className="inputGroupLabel" htmlFor="password">Contraseña:</label>
             </div>
-            <div>
-              <div>
-                <label htmlFor="email">Correo Electronico:</label>
-              </div>
-              <input type="email" id="email" name="email" required />
+            <div className="inputGroup">
+              <input className="inputGroupInput" type="email" id="email" name="email" required />
+              <label className="inputGroupLabel" htmlFor="email">Correo Electronico:</label>
             </div>
-            <div>
-              <div>
-                <label htmlFor="contact">Numero de contacto:</label>
-              </div>
-              <input type="text" id="contact" name="contact" required />
+            <div className="inputGroup">
+              <input className="inputGroupInput" type="text" id="contact" name="contact" required />
+              <label className="inputGroupLabel" htmlFor="contact">Numero de contacto:</label>
             </div>
-            <div>
-              <div>
-                <label htmlFor="fullName">Nombre completo:</label>
-              </div>
-              <input type="text" id="fullName" name="fullName" required />
+            <div className="inputGroup">
+              <input className="inputGroupInput" type="text" id="fullName" name="fullName" required />
+              <label className="inputGroupLabel" htmlFor="fullName">Nombre completo:</label>
             </div>
-            <div>
-              <div>
-                <label htmlFor="city">Especialidad:</label>
-              </div>
-
+            <div className="inputGroup">
               <select
                 id="speciality"
                 value={selectedSpeciality}
@@ -169,12 +152,9 @@ const SignUpUser = () => {
                   </option>
                 ))}
               </select>
+              <label className="inputGroupLabel" htmlFor="city">Especialidad:</label>
             </div>
-            <div>
-              <div>
-                <label htmlFor="city">Ciudad:</label>
-              </div>
-
+            <div className="inputGroup">
               <select
                 id="city"
                 value={selectedCity}
@@ -187,12 +167,11 @@ const SignUpUser = () => {
                   </option>
                 ))}
               </select>
+              <label className="inputGroupLabel" htmlFor="city">Ciudad:</label>
             </div>
-            <div>
-              <div>
-                <label htmlFor="experience">Experiencia previa:</label>
-              </div>
-             <textarea name="htmlFor" id="htmlFor" rows="4" cols="50" required>Cuentanos tu experiencia previa...</textarea>
+            <div className="inputGroup">
+             <textarea className="xp" name="htmlFor" id="htmlFor" rows="4" cols="50" placeholder="Cuentanos tu experiencia previa..." required></textarea>
+              <label className="inputGroupLabel" htmlFor="experience">Experiencia previa:</label>
             </div>
 
             <button type="submit">Registrarme</button>
